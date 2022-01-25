2009ec0377557d60e06633f47da06efb3b5317db
Mohamed-Fayaz-18 <96767659+Mohamed-Fayaz-18@users.noreply.github.com>
2022-01-25 10:35:50 +0530

    start help about

--- script.py
+++ script.py
@@ -14,13 +14,22 @@ I ᴀᴍ A Sɪᴍᴘʟᴇ Iᴍᴀɢᴇ Eᴅɪᴛᴏʀ Bᴏᴛ. I Cᴀɴ Eᴅɪ�
 <code>🌀 Yᴏᴜʀ Eᴅɪᴛᴇᴅ Iᴍᴀɢᴇ ᴡɪʟʟ ʙᴇ Uᴘʟᴏᴀᴅᴇᴅ</code>
 
 
-© @VKPROJECTS"""
-    ABOUT_MSG = """⭕️<b>My Name : Image Editor</b>
-
-⭕️<b>Channel :</b> <a href='https://t.me/VKPROJECTS'>VK PROJECTS</a>
-
-⭕️<b>Language :</b> <code>Python3</code>
-
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>
-
-⭕️<b>Creater :</b> <a href='t.me/VKPROJECTS'>VK PROJECTS</a>"""
+𝗕𝗢𝗧 𝗖𝗥𝗘𝗔𝗧𝗘𝗗 𝗔𝗡𝗗 𝗠𝗔𝗧𝗜𝗡𝗔𝗘𝗗 𝗕𝗬 @KOT_FREE_DE_LA_HOYA_OFF"""
+    ABOUT_MSG = """
+╭──────[@KOT_BOTS]───────〄
+│
+├ Nᴀᴍᴇ : <a href='https://t.me/KOT_IMAGE_EDITOR_BOT'>Kᴏᴛ Iᴍᴀɢᴇ Eᴅɪᴛᴏʀ Bᴏᴛ</a>
+│
+├ Sᴇʀᴠᴇʀ : <a href='https://heroku.com/'>Hᴇʀᴏᴋᴜ</a>
+│ 
+├ Lᴀɴɢᴜᴀɢᴇ : <a href='https://docs.pyrogram.org/'>Pʏᴛʜᴏɴ 3.9.6</a>
+│
+├ Vᴇʀꜱɪᴏɴ : <a href='https://t.me/KOT_IMAGE_EDITOR_BOT'>1.0 Bᴇᴛᴀ</a>
+│
+├ Fʀᴀᴍᴇᴡᴏʀᴋ : <a href='https://docs.pyrogram.org/'>Pʏʀᴏɢʀᴀᴍ 1.2.9</a>
+│
+├ Dᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/KOT_FREE_DE_LA_HOYA_OFF'>✯°• Kᴏᴛ Fʀᴇᴇ Dᴇ Lᴀ Hᴏʏᴀ Oғғ °•✯ | ✪ Bᴏᴛs CʀᴇᴀᴛᴏR ✪</a>
+│
+├ Pᴏᴡᴇʀᴇᴅ Bʏ : <a href='https://t.me/KOT_LINKS_TEAM'>Kᴏᴛ Lɪɴᴋs Tᴇᴀᴍ</a>
+│
+├ Uᴘᴅᴀᴛᴇᴅ Oɴ : [ 25.1.2022 ] 05:58 AM """
